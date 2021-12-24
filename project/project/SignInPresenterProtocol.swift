77e61572b289b3e9fe31fb25ebe86d72af7b0dc7
Yatskovets Dmitry <yatskovets.d@gmail.com>
2021-12-24 17:01:30 +0300

    start work on mvp

---
+++ project/project/SignInPresenterProtocol.swift
@@ -0,0 +1,10 @@
+//
+//  SignInPresenterProtocol.swift
+//  project
+//
+//  Created by Dzmitry on 24.12.21.
+//
+
+protocol SignInPresenterProtocol {
+    func logIn(email: String, password: String)
+}
